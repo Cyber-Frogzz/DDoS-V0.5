e204320d46cfc272bc639c8a51766a20e4f26a43
PhantomGuard-Frogzz <frogzzcyber@gmail.com>
2025-04-19 13:05:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,35 +3,16 @@ import os
 def menu():
     os.system('clear')
     print('''
-        Welcome To Tools Cyber-Frogzz
+        Welcome To Tools PhantomGuard Frogzz
 
 ===============================================
-
-
-[Author     : Frogzz          ]
-[Community  : 𝙋𝙝𝙖𝙣𝙩𝙤𝙢𝙂𝙪𝙖𝙧𝙙𓆃 ]
-[My Tiktok  : Frogzz          ]
-[My Github  : https://github.com/PhantomGuard-Frogzz/DDoS-V0.5]
-[My YouTube : frogzzsamp-88]
-
-                  ⚠️WARNING⚠️
-          
-          Hati Hati Saat Menggunakannya
-
+[1] Simulasi Traffic (700 request/10 detik)
 ===============================================
-                  Pilihan Menu
-
-[1] DDoS V0.5
-''')
+    ''')
     pilihan = input("Pilih menu: ")
-
     if pilihan == '1':
-        ip = input("IP Target (contoh: http://1.2.3.4): ")
-        
-        # Menjalankan DDoS dengan 50.000 request
-        os.system(f'python DDoS_V0.5.py {ip}')
-        
-        input("Tekan Enter untuk kembali ke menu...")
-        menu()
+        ip = input("Target (contoh: http://1.2.3.4): ")
+        os.system(f'python DDoS-V0.5.py {ip}')
+        input("Selesai! Tekan Enter untuk keluar.")
 
 menu()
