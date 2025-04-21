91327be6be1d731d177453f73cf9393f0dba061d
PhantomGuard-Frogzz <frogzzcyber@gmail.com>
2025-04-21 21:58:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,15 @@ def banner():
 def menu():
     print("\n[1] DDoS Attack")
     print("[2] IP Tracker")
+    print("[3] Check Server SA:MP")
     choice = input("\nPilih menu: ")
 
     if choice == "1":
         os.system("python AllTools.py")
     elif choice == "2":
         os.system("python AllTools.py track")
+    elif choice == "3":
+        os.system("python Check-Server.py")
     else:
         print("Pilihan tidak valid!")
 
